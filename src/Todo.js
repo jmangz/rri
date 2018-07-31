ffae9b887d0ab800690879a1fbc98296f0ba4c8e
jmangz <echou1@binghamton.edu>
2018-07-31 17:50:53 -0400

    added conditional rendering for todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Todo = () => {
-  
+const Todo = ({ todo }) => {
+  return todo.isDone ? (
+    <strike>
+      {todo.text}
+    </strike>) : (
+      <span>
+        {todo.text}
+      </span>);
+};
+
+Todo.propTypes = {
+  todo: PropTypes.object.isRequired,
 };
 
 export default Todo;
