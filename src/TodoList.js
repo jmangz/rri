80fdb919cf9f2833d62c3f19ce3a1752d2af441b
jmangz <echou1@binghamton.edu>
2018-07-31 17:39:06 -0400

    added prop-types to dependencies and todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Todo from './Todo';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => (
+  <div className="todo">
+    <input type="text" placeholder="Add todo" />
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <li key={todo.id} className="todo-item">
+          <Todo todo={todo} />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.objects).isRequired,
 };
-
 export default TodoList;
