3cd968cc2534bfe381ed9e71cfa67f7214225817
jmangz <echou1@binghamton.edu>
2018-08-22 12:27:12 -0400

    finialized webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
